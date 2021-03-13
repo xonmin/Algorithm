df1c62fd95424f69bd76f8daf7be46eb838ebc14
Xonmin <han51361@naver.com>
2021-03-13 22:15:45 +0900

    Create Q4.java
    0313 네 번째 문제

---
+++ src/com/company/W6/Q4.java
@@ -0,0 +1,23 @@
+public class Q4{
+  // 1이 될 
+  public void exe(){
+    Scanner sc =  new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int count = 0;
+		while(true){
+			if(n == 1){
+				break;
+			}
+			if(n%k ==0){
+				count ++;
+				n = n/k;
+			}else{
+			n -=1;
+			count++;
+			}
+		}
+		System.out.println(count);
+  }
+  
+}
