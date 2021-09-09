50c5b3238940e6f102905d5da5c8a0cdff1cb92f
xonmin <han51361@naver.com>
2021-09-09 16:06:02 +0900

    9월 9일 일곱 난쟁이

--- pythonProject/collegeBaseClass/sevenDwarf.py
+++ pythonProject/collegeBaseClass/sevenDwarf.py
@@ -1,6 +1,23 @@
+from itertools import combinations
+
+# 2309번 일곱 난쟁이
+
 dwarf = list()
 
-for _ in range(9):
-    dwarf.append(map(int,input()))
+for i in range(9):
+    dwarf.append(int(input()))
 
-print(dwarf)
+dwarf_comb = combinations(dwarf,7)
+
+ans = []
+for sum100 in dwarf_comb:
+    if sum(sum100) == 100:
+        ans = list(sum100)
+        break
+    else:
+        continue
+
+ans.sort()
+
+for i in ans:
+    print(i)
