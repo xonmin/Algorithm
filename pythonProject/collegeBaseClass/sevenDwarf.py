7c5cbbe645afe7006cf72623f8ec12c4aa80a3f3
xonmin <han51361@naver.com>
2021-09-09 16:05:48 +0900

    9월 9일 일곱 난쟁이

---
+++ pythonProject/collegeBaseClass/sevenDwarf.py
@@ -0,0 +1,6 @@
+dwarf = list()
+
+for _ in range(9):
+    dwarf.append(map(int,input()))
+
+print(dwarf)
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,23 @@
-# 1152번 단어의 개수
+from itertools import combinations
 
+# 2309번 일곱 난쟁이
 
+dwarf = list()
 
-sentence =  input().split()
+for i in range(9):
+    dwarf.append(int(input()))
 
-word_count = len(sentence)
+dwarf_comb = combinations(dwarf,7)
 
-print(word_count)
+ans = []
+for sum100 in dwarf_comb:
+    if sum(sum100) == 100:
+        ans = list(sum100)
+        break
+    else:
+        continue
 
+ans.sort()
+
+for i in ans:
+    print(i)
