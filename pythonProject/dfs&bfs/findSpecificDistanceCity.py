26a852607738b58619f60955b41b0cbe42fc4e49
xonmin <han51361@naver.com>
2021-09-12 23:24:00 +0900

    9월 12일 특정 거리 만족하는 도시 찾기

---
+++ pythonProject/dfs&bfs/findSpecificDistanceCity.py
@@ -0,0 +1,4 @@
+N , M , K , X = map(int, input().split())
+
+
+print(N,M,K,X)
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,58 +1,36 @@
-import re
+from collections import deque
+N , M , K , X = map(int, input().split())
+load_list = [list(map(int,input().split())) for _ in range(M)]
+
+#로직
+
+q = deque()
+visited = [False] * (N+1)
+satisfied_city = [] # 조건을 만족하는 도시
+
+#print("거리 정보",load_list)
+q.append((X,0))
+visited[X] = True
+count = 0
+reach = 0
+while q:
+    city, count = q.popleft()
+    if count == K:
+        satisfied_city.append(city)
+    elif count < K:
+        for start, end in load_list:
+            if start == city and not visited[end] :
+                visited[end] = True
+                q.append((end, count +1))
+
+if len(satisfied_city) == 0:
+    print(-1)
+else:
+    satisfied_city.sort()
+    for city in satisfied_city:
+            print(city)
 
 
-def solution(s):
-    answer = 0
-    temp_ans = 999999
-
-    # 문자열 길이가 1일 때
-    if (len(s) == 1):
-        answer = 1
-        #print(answer)
-        return answer
-
-    for i in range(1, len(s)):
-        compressed_list = list()
-        tokenization_list = [s[j:j + i] for j in range(0, len(s), i)]
-
-        compressed_list.append([1, tokenization_list[0]])
-        length = len(tokenization_list)
-
-        for i in range(1, length):
-            compare_beforeValue = compressed_list.pop()
-            compressed_list.append(compare_beforeValue)
-            # 이전 값이랑 문자가 같으면
-            if tokenization_list[i] == compare_beforeValue[1]:
-                # 기존 꺼 제거 , 값 증가
-                compressed_list.pop()
-                compare_beforeValue[0] += 1
-                compressed_list.append(compare_beforeValue)
-            # print(compressed_list)
-            else:
-                compressed_list.append([1, tokenization_list[i]])
-            # print(compressed_list)
-
-        my_list = sum(compressed_list, [])
-
-        pressed_string = "".join(map(str, my_list))
-        # 1제거
-
-        try:
-            for j in range(0, len(pressed_string) - 1):
-                #print(re.findall("[a-zA-Z]", pressed_string[j + 1]))
-                if pressed_string[j] == "1" and re.findall("[a-zA-Z]", pressed_string[j + 1]) != []:
-                    pressed_string = pressed_string[:j] + pressed_string[j+1:]
-                    #print(j,'번째',"1제거하러", pressed_string)
-            #print(pressed_string)
-
-        except:
-            pass
 
 
-        temp_ans = min(temp_ans, len(pressed_string))
-    print(temp_ans)
-    answer = temp_ans
-    return answer
 
-
-solution("aaaaaaaaaabb")
