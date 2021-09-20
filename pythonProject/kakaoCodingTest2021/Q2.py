2d0264bdaf1163dbf8ecc0cf085ae58b9de955c6
xonmin <han51361@naver.com>
2021-09-20 17:53:42 +0900

    9월 20일 카카오2021 코테 2번

--- pythonProject/kakaoCodingTest2021/Q2.py
+++ pythonProject/kakaoCodingTest2021/Q2.py
@@ -1,30 +1,37 @@
-문제 설명
-문제 설명
-양의 정수 n이 주어집니다. 이 숫자를 k진수로 바꿨을 때, 변환된 수 안에 아래 조건에 맞는 소수(Prime number)가 몇 개인지 알아보려 합니다.
 
-0P0처럼 소수 양 쪽에 0이 있는 경우
-P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
-0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
-P처럼 소수 양쪽에 아무것도 없는 경우
-단, P는 각 자릿수에 0을 포함하지 않는 소수입니다.
-예를 들어, 101은 P가 될 수 없습니다.
-예를 들어, 437674을 3진수로 바꾸면 211020101011입니다. 여기서 찾을 수 있는 조건에 맞는 소수는 왼쪽부터 순서대로 211, 2, 11이 있으며, 총 3개입니다. 211은 P0 형태에서 찾을 수 있으며, 2는 0P0에서, 11은 0P에서 찾을 수 있습니다.
+# 카카오 코딩테스트 2번 문제
+import string
 
-정수 n과 k가 매개변수로 주어집니다. n을 k진수로 바꿨을 때, 변환된 수 안에서 찾을 수 있는 위 조건에 맞는 소수의 개수를 return 하도록 solution 함수를 완성해 주세요.
 
-제한사항
-1 ≤ n ≤ 1,000,000
-3 ≤ k ≤ 10
-입출력 예
-n	k	result
-437674	3	3
-110011	10	2
-입출력 예 설명
-입출력 예 #1
+def change(n, k):
+    tmp = string.digits + string.ascii_lowercase
+    q, r = divmod(n, k)
+    if q == 0:
+        return tmp[r]
+    else:
+        return change(q, k) + tmp[r]
 
-문제 예시와 같습니다.
 
-입출력 예 #2
+def check_prime_num(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
-110011을 10진수로 바꾸면 110011입니다. 여기서 찾을 수 있는 조건에 맞는 소수는 11, 11 2개입니다. 이와 같이, 중복되는 소수를 발견하더라도 모두 따로 세어야 합니다.
 
+def soltion(n, k):
+    change_k = change(n, k)
+    candi_prime = change_k.split('0')
+    while '' in candi_prime:
+        candi_prime.remove('')
+    candi_prime = list(map(int,candi_prime))
+
+    ans = 0
+    for prime_num in candi_prime:
+        if check_prime_num(prime_num):
+            ans += 1
+    return ans
+
+print(soltion(110011, 10))
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,69 +1,36 @@
-# 백준 14502번 및 이코테
-import sys
-import copy
-from collections import deque
-from itertools import combinations
-
-#
-# 7 7
-# 2 0 0 0 1 1 0
-# 0 0 1 0 1 2 0
-# 0 1 1 0 1 0 0
-# 0 1 0 0 0 0 0
-# 0 0 0 0 0 1 1
-# 0 1 0 0 0 0 0
-# 0 1 0 0 0 0 0
-N, M = map(int, sys.stdin.readline().split())
-map = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-copy_map = map
-# logic
+# 카카오 코딩테스트 2번 문제
+import string
 
 
-
-rangeX = [0, 1, 0, -1]
-rangeY = [-1, 0, 1, 0]
-ans = 0
-zero_zone = []
-virus_zone = []
-
-for i in range(N):
-    for j in range(M):
-        if map[i][j] == 0:
-            zero_zone.append([i, j])
-        if map[i][j] == 2:
-            virus_zone.append([i, j])
-# 벽 3개 고르기
-candidate_wall_list = combinations(zero_zone, 3)
+def change(n, k):
+    tmp = string.digits + string.ascii_lowercase
+    q, r = divmod(n, k)
+    if q == 0:
+        return tmp[r]
+    else:
+        return change(q, k) + tmp[r]
 
 
-def virus(x, y):
-    q = deque()
-    q.append([x, y])
-    visit = [[False] * M for _ in range(N)]
-    visit[x][y] = True
-    while q:
-        now_x, now_y = q.popleft()
-        for i in range(4):
-            temp_x = now_x + rangeX[i]
-            temp_y = now_y + rangeY[i]
-            if 0 <= temp_x < N and 0 <= temp_y < M and visit[temp_x][temp_y] is False:
-                if copy_map[temp_x][temp_y] == 0:
-                    copy_map[temp_x][temp_y] = 2
-                    visit[temp_x][temp_y] = True
-                    q.append([temp_x, temp_y])
+def check_prime_num(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 
-for wall_list in candidate_wall_list:
-    copy_map = copy.deepcopy(map)
+def soltion(n, k):
+    change_k = change(n, k)
+    candi_prime = change_k.split('0')
+    while '' in candi_prime:
+        candi_prime.remove('')
+    candi_prime = list(map(int,candi_prime))
 
-    #벽세워
-    for i in range(3):
-        copy_map[wall_list[i][0]][wall_list[i][1]] = 1
-    #바이러스 뿌려 -> 맵 바꿔
-    for spread in virus_zone:
-        virus(spread[0], spread[1])
-    count_zero = sum(i.count(0) for i in copy_map)
-    #print("조회" ,count_zero)
-    ans = max(count_zero, ans)
+    ans = 0
+    for prime_num in candi_prime:
+        if check_prime_num(prime_num):
+            ans += 1
+    return ans
 
-print(ans)
+print(soltion(110011, 10))
