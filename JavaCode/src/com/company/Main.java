05b9aae037d82275c224ba5909bf842687237655
xonmin <han51361@naver.com>
2021-09-29 15:30:47 +0900

    9월 29일 (수) zoa스터디 문제 풀기

--- JavaCode/src/com/company/Main.java
+++ JavaCode/src/com/company/Main.java
@@ -43,8 +43,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Q3 q3 = new Q3();
-        int[] fees = {180, 5000, 10, 600};
-        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+        int[] fees = {1, 461, 1, 10};
+        String[] records = {"00:00 1234 IN"};
         int[] ans = q3.solution(fees, records);
         for (int k : ans) {
             System.out.println(k);
--- pythonProject/wordStudy.py
+++ pythonProject/wordStudy.py
@@ -1,20 +1,18 @@
-#1157번 단어 공부
+# 1157번 단어 공부
 
 import sys
 import string
+
 input_string = sys.stdin.readline().rstrip()
 upper = input_string.upper()
 set_input_str = set(upper)
 
-
-
-word_list = {i : 0 for i in upper}
-
+word_list = {i: 0 for i in upper}
 
 for c in upper:
     word_list[c] += 1
 
-ans = [k for k,v in word_list.items() if max(word_list.values()) == v]
+ans = [k for k, v in word_list.items() if max(word_list.values()) == v]
 if len(ans) > 1:
     print('?')
 else:
