c4ae6dcd4656ce70dbdf28446b8479e97fc10e23
han51361 <han51361@gmail.com>
2021-10-25 15:35:18 +0900

    Q1694 complete

--- pythonProject/dfs&bfs/hideAndSeek.py
+++ pythonProject/dfs&bfs/hideAndSeek.py
@@ -1,7 +1,36 @@
 import sys
+from collections import deque
+
+# 1697번 숨바꼭질
 
 
-N , K =  map(int, sys.stdin.readline().split())
+N, K = map(int, sys.stdin.readline().split())
+
+rng = [-1, 1, N]
+
+chk =  [0 * _ for _ in range(100001)]
+
+def findBrother(N, K):
+
+    if N == K:
+        return 0
+
+    q = deque()
+    q.append([N, 0])
+    while q:
+        now_pos,count = q.popleft()
+        temp_position = [now_pos+1,now_pos-1,2*now_pos]
+        for temp_pos in temp_position:
+            if 0 <= temp_pos < len(chk) and chk[temp_pos] == 0:
+                temp_count = count + 1
+                chk[temp_pos] = temp_count
+                if temp_pos == K:
+                    return temp_count
+                else:
+                    q.append([temp_pos, temp_count])
 
 
-print(N, K)
+case1 = findBrother(N,K)
+
+print(case1)
+
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -8,32 +8,29 @@ N, K = map(int, sys.stdin.readline().split())
 
 rng = [-1, 1, N]
 
+chk =  [0 * _ for _ in range(100001)]
 
 def findBrother(N, K):
 
     if N == K:
-        return 1
+        return 0
 
     q = deque()
-    q.append([N, 1])
+    q.append([N, 0])
     while q:
         now_pos,count = q.popleft()
         temp_position = [now_pos+1,now_pos-1,2*now_pos]
         for temp_pos in temp_position:
-            if 0 <= temp_pos <= 100000:
+            if 0 <= temp_pos < len(chk) and chk[temp_pos] == 0:
                 temp_count = count + 1
+                chk[temp_pos] = temp_count
                 if temp_pos == K:
                     return temp_count
                 else:
                     q.append([temp_pos, temp_count])
 
 
-case1 = findBrother(N+1,K)
-#print(case1)
-case2 = findBrother(N-1,K)
-#print(case2)
-case3 = findBrother(2*N,K)
-#print(case3)
-ans = min(case1,min(case2,case3))
+case1 = findBrother(N,K)
 
-print(ans)
+print(case1)
+
