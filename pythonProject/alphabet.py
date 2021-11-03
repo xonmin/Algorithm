14dab992389c22dda20fe973921a027671cf7773
han51361 <han51361@gmail.com>
2021-11-03 12:46:59 +0900

    1103 알고리즘 스터디 1987번 알파벳 수정

--- pythonProject/alphabet.py
+++ pythonProject/alphabet.py
@@ -6,32 +6,29 @@ import copy
 
 r, c = map(int, sys.stdin.readline().split())
 
-board = [list(sys.stdin.readline().rstrip()) for _ in range(r)]
+board = [list(map(lambda x : ord(x)-65,sys.stdin.readline().rstrip())) for _ in range(r)]
 #테스트
 rangeX = [0, 1, 0, -1]
 rangeY = [-1, 0, 1, 0]
-visited = [[0] * c for _ in range(r)]
-
-max_count = 0
-
-foot_print = list()
-
+ch = [0] * 26
 
 def dfs(fp, x, y):
+    global ans
+    ans = max(ans,fp)
+
     for i in range(4):
         temp_x = x + rangeX[i]
         temp_y = y + rangeY[i]
-        new_fp = copy.deepcopy(fp)
-        if 0 <= temp_x < r and 0 <= temp_y < c:
-            if board[temp_x][temp_y] not in fp:
-                new_fp.append(board[temp_x][temp_y])
-                visited[temp_x][temp_y] = visited[x][y] + 1
-                dfs(new_fp, temp_x, temp_y)
+
+        if 0 <= temp_x < r and 0 <= temp_y < c and ch[board[temp_x][temp_y]] == 0 :
+            ch[board[temp_x][temp_y]] = 1
+            dfs(fp+1, temp_x, temp_y)
+            ch[board[temp_x][temp_y]] = 0
 
 
-visited[0][0] = 1
-foot_print.append(board[0][0])
-dfs(foot_print, 0, 0)
+ch[board[0][0]] = 1
+ans = 1
+dfs(ans, 0, 0)
 
 # print(visited)
-print(max(map(max, visited)))
+print(ans)
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,34 +1,34 @@
-#더하기 사이클 Q1110
-
 import sys
+from collections import deque
+import copy
+
+# 백준 1987번 알파벳
+
+r, c = map(int, sys.stdin.readline().split())
+
+board = [list(map(lambda x : ord(x)-65,sys.stdin.readline().rstrip())) for _ in range(r)]
+#테스트
+rangeX = [0, 1, 0, -1]
+rangeY = [-1, 0, 1, 0]
+ch = [0] * 26
+
+def dfs(fp, x, y):
+    global ans
+    ans = max(ans,fp)
+
+    for i in range(4):
+        temp_x = x + rangeX[i]
+        temp_y = y + rangeY[i]
+
+        if 0 <= temp_x < r and 0 <= temp_y < c and ch[board[temp_x][temp_y]] == 0 :
+            ch[board[temp_x][temp_y]] = 1
+            dfs(fp+1, temp_x, temp_y)
+            ch[board[temp_x][temp_y]] = 0
 
 
-N = int(sys.stdin.readline())
+ch[board[0][0]] = 1
+ans = 1
+dfs(ans, 0, 0)
 
-
-
-contrast = 0
-ans = 0
-
-new_int = N
-if N == 0 :
-    print(1)
-    exit(0)
-
-while N != contrast:
-    if new_int < 10:
-        first = 0
-        second = new_int
-    else:
-        first = new_int // 10
-        #print(first)
-        second = new_int % 10
-        #print(second)
-
-    new_int = second*10 + (first+second)%10
-    contrast = new_int
-    #print(contrast)
-    ans += 1
-
-
+# print(visited)
 print(ans)
