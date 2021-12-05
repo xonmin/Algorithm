e7ea61a3f4e1ebf60bc0405ebdf20605fa077750
han51361 <han51361@gmail.com>
2021-12-05 15:14:42 +0900

    1205(수) - 알고리즘 백준 1057 토너먼트 (브루트포스)

--- pythonProject/Bjoon/tournament.py
+++ pythonProject/Bjoon/tournament.py
@@ -6,10 +6,11 @@ def solution(n, k, l):
     count = 0
 
     while k != l:
-        k -= k //2
-        l -= l//2
-        count +=1
+        k -= k // 2
+        l -= l // 2
+        count += 1
     return count
+
 
 n, k, l = map(int, sys.stdin.readline().split())
 
