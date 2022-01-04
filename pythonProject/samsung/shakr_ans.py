4205f648cc238afba47e34088ec686f8e65a9e35
han51361 <han51361@gmail.com>
2022-01-04 15:55:13 +0900

    0104(화) - 아기상어_Q16236(삼성SW- bfs)

---
+++ pythonProject/samsung/shakr_ans.py
@@ -0,0 +1,70 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+
+s_x, s_y = -1, -1
+s_level, eat = 2, 0
+graph = []
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    graph.append(data)
+    for j in range(n):
+        if data[j] == 9:
+            s_x, s_y = i, j
+
+graph[s_x][s_y] = 0
+
+
+def bfs(s_x, s_y, s_level):
+    visited = [[False] * n for _ in range(n)]
+    q = deque()
+    q.append((s_x, s_y, 0))
+    visited[s_x][s_y] = True
+    fish = []
+    while q:
+        x, y, count = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                visited[nx][ny] = True
+                if graph[nx][ny] != 0 and graph[nx][ny] < s_level:
+                    fish.append((count + 1, nx, ny))
+                    q.append((nx, ny, count + 1))
+                    visited[nx][ny] = True
+                elif graph[nx][ny] == 0 or graph[nx][ny] == s_level:
+                    visited[nx][ny] = True
+                    q.append((nx, ny, count + 1))
+
+    fish.sort()
+    if fish:
+        return [fish[0][1], fish[0][2], fish[0][0]]
+    else:
+        return []
+
+
+answer = 0
+
+while True:
+    fish_eat = bfs(s_x, s_y, s_level)
+    if fish_eat:
+        x, y, move = fish_eat
+        graph[x][y] = 0
+        eat += 1
+        answer += move
+        if eat == s_level:
+            s_level += 1
+            eat = 0
+        s_x, s_y = x, y
+    else:
+        break
+
+print(answer)
+
+
+
+
--- pythonProject/samsung/smallShark.py
+++ pythonProject/samsung/smallShark.py
@@ -2,46 +2,62 @@ import sys
 from collections import deque
 
 
-def bfs(x, y, n, sea):
+def bfs(x, y, size):
     rangeX = [0, 1, 0, -1]
     rangeY = [-1, 0, 1, 0]
 
+    visited = [[False] * n for _ in range(n)]
     q = deque()
-    q.append([x, y, 0, 2])
-    visited = [[-1] * n for i in range(n)]
-    visited[x][y] = -2
+    q.append([x, y, 0])
+    visited[x][y] = True
+    feed = []
 
     while q:
-        feed = len(q)
-        q = deque(sorted(q))
-
-        now_x, now_y, count, size = q.popleft()
-
+        x, y, count = q.popleft()
         for i in range(4):
-            temp_x = now_x + rangeX[i]
-            temp_y = now_y + rangeY[i]
-            if 0 <= temp_x < n and 0 <= temp_y < n and visited[temp_x][temp_y] is -1:
-                if sea[temp_x][temp_y] > size:
-                    continue
-                else:
-                    if sea[temp_x][temp_y] == size:
-                        size += 1
-                    count += 1
-                    visited[temp_x][temp_y] = min()
-                    q.append([temp_x, temp_y, count, size])
-    if min_time == 999999999:
-        min_time = 0
-    return min_time
+            nx = x + rangeX[i]
+            ny = y + rangeY[i]
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                visited[nx][ny] = True
+                if sea[nx][ny] != 0 and sea[nx][ny] < size:
+                    feed.append([nx, ny, count + 1])
+                    q.append([nx, ny, count + 1])
+                    visited[nx][ny] = True
+                elif sea[nx][ny] == 0 or sea[nx][ny] == size:
+                    visited[nx][ny] = True
+                    q.append([nx, ny, count + 1])
+    feed.sort(key=lambda x : (x[2],x[0],x[1]))
+    if feed:
+        return [feed[0][0], feed[0][1], feed[0][2]]
+    else:
+        return []
 
 
 def solution(n):
+    size = 2
+    global sea
     sea = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
-
+    ans = 0
+    feed = 0
+    x, y = 0, 0
     for i in range(n):
         for j in range(n):
             if sea[i][j] == 9:
-                ans = bfs(i, j, n, sea)
-
+                x, y = i, j
+    while True:
+        fish_eat = bfs(x, y, size)
+        print(fish_eat)
+        if fish_eat:
+            new_x, new_y, count = fish_eat
+            sea[new_x][new_y] = 0
+            feed += 1
+            ans += count
+            if feed == size:
+                size += 1
+                feed = 0  # 먹은거 초기화
+            x, y = new_x, new_y
+        else:
+            break
     print(ans)
 
 
